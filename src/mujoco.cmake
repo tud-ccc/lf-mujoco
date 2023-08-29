4f494543d7de290f03787197f18f8ae54511a703
revol-xut <revol-xut@protonmail.com>
2023-08-29 13:04:37 +0200

    implementing bodyless reactions

--- src/mujoco.cmake
+++ src/mujoco.cmake
@@ -6,5 +6,6 @@ link_libraries(mujoco::mujoco)
 
 set(CPP_SOURCES ${CMAKE_CURRENT_LIST_DIR})
 
-target_link_libraries(${LF_MAIN_TARGET} ${CMAKE_THREAD_LIBS_INIT} mujoco::mujoco glfw)
 
+target_link_libraries(${LF_MAIN_TARGET} ${CMAKE_THREAD_LIBS_INIT} mujoco::mujoco glfw)
+target_sources(${LF_MAIN_TARGET} PRIVATE ${CMAKE_CURRENT_LIST_DIR}/mujoco_simulator.cpp)
