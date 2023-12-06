4a4508c951bcd0ea4cbdbe830d34e16c4a015b57
revol-xut <revol-xut@protonmail.com>
2023-12-06 11:31:21 +0100

    setting opengl preference

--- examples/mujoco.cmake
+++ examples/mujoco.cmake
@@ -1,11 +1,13 @@
-find_package (Threads)
-find_package (glfw)
+#set(OpenGL_GL_PREFERENC LEGACY)
+SET(CXX_STANDARD 17)
+
+find_package (Threads REQUIRED)
+find_package (glfw3 REQUIRED)
 
 find_package(mujoco REQUIRED)
-link_libraries(mujoco::mujoco)
+#link_libraries(mujoco::mujoco)
 
 set(CPP_SOURCES ${CMAKE_CURRENT_LIST_DIR})
-
 
 target_link_libraries(${LF_MAIN_TARGET} ${CMAKE_THREAD_LIBS_INIT} mujoco::mujoco glfw)
 target_sources(${LF_MAIN_TARGET} PRIVATE ${CMAKE_CURRENT_LIST_DIR}/../src/mujoco_simulator.cpp)
