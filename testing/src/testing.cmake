0f17c2d71759174a172c73cb0f270b3606996f30
PmupHutt <benedict.mehnert@gmail.com>
2024-04-19 14:52:32 +0200

    feat: rerder testing structure

--- testing/src/testing.cmake
+++ testing/src/testing.cmake
@@ -1,3 +1,5 @@
 SET(CXX_STANDARD 17)
 # in order to make a file visible in the whole target we can update the include path using this command
 target_include_directories(${LF_MAIN_TARGET} PRIVATE ${CMAKE_CURRENT_LIST_DIR}/../../src/common/)
+
+target_include_directories(${LF_MAIN_TARGET} PRIVATE ${CMAKE_CURRENT_LIST_DIR}/src/test_common/)
