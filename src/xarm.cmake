44798e2e78bcc0bc6e6df9670225a798b6348d69
PmupHutt <benedict.mehnert@gmail.com>
2024-04-24 16:06:20 +0200

    feat: adjust cmkae files to include the cpp files

--- src/xarm.cmake
+++ src/xarm.cmake
@@ -8,3 +8,6 @@ target_include_directories(${LF_MAIN_TARGET} PUBLIC "${X_ARM_INCLUDE_PATH}")
 
 # in order to make a file visible in the whole target we can update the include path using this command
 target_include_directories(${LF_MAIN_TARGET} PRIVATE ${CMAKE_CURRENT_LIST_DIR}/common/)
+# in order to include also the .cpp files we execute these two commands
+ADD_LIBRARY(cpp_files ${CMAKE_CURRENT_LIST_DIR}/common/common_vector.cpp)
+target_link_libraries(${LF_MAIN_TARGET} cpp_files)
--- testing/testing.cmake
+++ testing/testing.cmake
@@ -1,5 +1,9 @@
 SET(CXX_STANDARD 17)
 # in order to make a file visible in the whole target we can update the include path using this command
 target_include_directories(${LF_MAIN_TARGET} PRIVATE ${CMAKE_CURRENT_LIST_DIR}/../src/common/)
+# in order to include also the .cpp files we execute these two commands
+ADD_LIBRARY(cpp_files ${CMAKE_CURRENT_LIST_DIR}/../src/common/common_vector.cpp)
+target_link_libraries(${LF_MAIN_TARGET} cpp_files)
+
 
 target_include_directories(${LF_MAIN_TARGET} PRIVATE ${CMAKE_CURRENT_LIST_DIR}/test_common/)
