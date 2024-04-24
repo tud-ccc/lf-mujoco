76489e1989f8eec765016dd5bb607ef2c8d9a422
PmupHutt <benedict.mehnert@gmail.com>
2024-04-24 16:57:59 +0200

    fix: panics if finds nan

--- src/common/common_error_handling.hpp
+++ src/common/common_error_handling.hpp
@@ -6,18 +6,18 @@
 #include <cmath>
 
 void assert_for_NULLs(Vector vec1, Vector vec2) {
-  assert(std::isnan(vec1.X_) && std::isnan(vec1.Y_) && std::isnan(vec1.Z_));
-  assert(std::isnan(vec2.X_) && std::isnan(vec2.Y_) && std::isnan(vec2.Z_));
+  assert(!std::isnan(vec1.X_) && !std::isnan(vec1.Y_) && !std::isnan(vec1.Z_));
+  assert(!std::isnan(vec2.X_) && !std::isnan(vec2.Y_) && !std::isnan(vec2.Z_));
   return;
 }
 
 void assert_for_NULLs(Vector vec) {
-  assert(std::isnan(vec.X_) && std::isnan(vec.Y_) && std::isnan(vec.Z_));
+  assert(!std::isnan(vec.X_) && !std::isnan(vec.Y_) && !std::isnan(vec.Z_));
   return;
 }
 
 void assert_for_NULLs(double value) {
-  assert(std::isnan(value));
+  assert(!std::isnan(value));
   return;
 }
 
