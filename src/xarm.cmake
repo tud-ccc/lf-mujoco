6902a4f59081770d99e2b330a06791eeed3352f8
PmupHutt <benedict.mehnert@gmail.com>
2024-04-24 16:58:34 +0200

    feat: avoid name collisions of targets if ./clean was not executed

--- src/xarm.cmake
+++ src/xarm.cmake
@@ -9,8 +9,8 @@ target_include_directories(${LF_MAIN_TARGET} PUBLIC "${X_ARM_INCLUDE_PATH}")
 # in order to make a file visible in the whole target we can update the include path using this command
 target_include_directories(${LF_MAIN_TARGET} PRIVATE ${CMAKE_CURRENT_LIST_DIR}/common/)
 # in order to include also the .cpp files we execute these two commands
-ADD_LIBRARY(cpp_files 
+ADD_LIBRARY(cpp_files_main 
     ${CMAKE_CURRENT_LIST_DIR}/common/common_vector.cpp
     ${CMAKE_CURRENT_LIST_DIR}/common/common_position_evaluator.cpp
     ${CMAKE_CURRENT_LIST_DIR}/common/common_world_data.cpp)
-target_link_libraries(${LF_MAIN_TARGET} cpp_files)
+target_link_libraries(${LF_MAIN_TARGET} cpp_files_main)
--- testing/testing.cmake
+++ testing/testing.cmake
@@ -2,11 +2,11 @@ SET(CXX_STANDARD 17)
 # in order to make a file visible in the whole target we can update the include path using this command
 target_include_directories(${LF_MAIN_TARGET} PRIVATE ${CMAKE_CURRENT_LIST_DIR}/../src/common/)
 # in order to include also the .cpp files we execute these two commands
-ADD_LIBRARY(cpp_files 
+ADD_LIBRARY(cpp_files_testing 
     ${CMAKE_CURRENT_LIST_DIR}/../src/common/common_vector.cpp
     ${CMAKE_CURRENT_LIST_DIR}/../src/common/common_position_evaluator.cpp
     ${CMAKE_CURRENT_LIST_DIR}/../src/common/common_position_evaluator.cpp)
-target_link_libraries(${LF_MAIN_TARGET} cpp_files)
+target_link_libraries(${LF_MAIN_TARGET} cpp_files_testing)
 
 
 target_include_directories(${LF_MAIN_TARGET} PRIVATE ${CMAKE_CURRENT_LIST_DIR}/test_common/)
