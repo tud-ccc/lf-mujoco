4183e40381c6b7d59b7c3bf197f212f3a2b017b6
PmupHutt <benedict.mehnert@gmail.com>
2024-04-26 20:34:05 +0200

    feat: new cmake layout for librariy linking

--- src/xarm.cmake
+++ src/xarm.cmake
@@ -6,11 +6,18 @@ target_link_libraries(${LF_MAIN_TARGET} "${X_ARM_LIB}")
 find_path(X_ARM_INCLUDE_PATH xarm)
 target_include_directories(${LF_MAIN_TARGET} PUBLIC "${X_ARM_INCLUDE_PATH}")
 
-# in order to make a file visible in the whole target we can update the include path using this command
-target_include_directories(${LF_MAIN_TARGET} PRIVATE ${CMAKE_CURRENT_LIST_DIR}/common/)
-# in order to include also the .cpp files we execute these two commands
-ADD_LIBRARY(cpp_files_main 
+# add cpp files to library
+ADD_LIBRARY(cpp_files_common
     ${CMAKE_CURRENT_LIST_DIR}/common/common_vector.cpp
     ${CMAKE_CURRENT_LIST_DIR}/common/common_position_evaluator.cpp
     ${CMAKE_CURRENT_LIST_DIR}/common/common_world_data.cpp)
-target_link_libraries(${LF_MAIN_TARGET} cpp_files_main)
+
+# add hpp files to library
+target_include_directories(cpp_files_common PRIVATE ${CMAKE_CURRENT_LIST_DIR}/common/)
+
+# link the library 
+target_link_libraries(${LF_MAIN_TARGET} cpp_files_common)
+
+# make the library visible also for the .lf files
+target_include_directories(${LF_MAIN_TARGET} PRIVATE ${CMAKE_CURRENT_LIST_DIR}/common/)
+
--- testing/testing.cmake
+++ testing/testing.cmake
@@ -1,12 +1,21 @@
 SET(CXX_STANDARD 17)
-# in order to make a file visible in the whole target we can update the include path using this command
-target_include_directories(${LF_MAIN_TARGET} PRIVATE ${CMAKE_CURRENT_LIST_DIR}/../src/common/)
-# in order to include also the .cpp files we execute these two commands
-ADD_LIBRARY(cpp_files_testing 
+
+# add cpp files to library
+ADD_LIBRARY(testing_cpp_files_common
     ${CMAKE_CURRENT_LIST_DIR}/../src/common/common_vector.cpp
     ${CMAKE_CURRENT_LIST_DIR}/../src/common/common_position_evaluator.cpp
-    ${CMAKE_CURRENT_LIST_DIR}/../src/common/common_position_evaluator.cpp)
-target_link_libraries(${LF_MAIN_TARGET} cpp_files_testing)
+    ${CMAKE_CURRENT_LIST_DIR}/../src/common/common_position_evaluator.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/test_common/random_vectors_handler.cpp)
+
+# add hpp files to library
+target_include_directories(testing_cpp_files_common PRIVATE ${CMAKE_CURRENT_LIST_DIR}/test_common/ ${CMAKE_CURRENT_LIST_DIR}/../src/common/)
+
+# link the library 
+target_link_libraries(${LF_MAIN_TARGET} testing_cpp_files_common)
+
+# make the library visible also for the .lf files
+target_include_directories(${LF_MAIN_TARGET} PRIVATE ${CMAKE_CURRENT_LIST_DIR}/test_common/ ${CMAKE_CURRENT_LIST_DIR}/../src/common/)
 
 
-target_include_directories(${LF_MAIN_TARGET} PRIVATE ${CMAKE_CURRENT_LIST_DIR}/test_common/)
+
+
