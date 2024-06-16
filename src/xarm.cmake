f943dc309d569d68195f7f3b990a426c30cbec55
PmupHutt <benedict.mehnert@gmail.com>
2024-06-16 11:52:51 +0200

    feat: make cmake understandable

--- src/xarm.cmake
+++ src/xarm.cmake
@@ -9,13 +9,7 @@ find_package(mujoco REQUIRED)
 find_library(X_ARM_LIB xarm)
 find_package(Eigen3 REQUIRED)
 
-
-#link_libraries(mujoco::mujoco)
-
-target_link_libraries(${LF_MAIN_TARGET} ${CMAKE_THREAD_LIBS_INIT} mujoco::mujoco glfw "${X_ARM_LIB}")
-
 find_path(X_ARM_INCLUDE_PATH xarm)
-target_include_directories(${LF_MAIN_TARGET} PUBLIC "${X_ARM_INCLUDE_PATH}")
 
 # add cpp files to library
 ADD_LIBRARY(cpp_files_common
@@ -26,12 +20,19 @@ ADD_LIBRARY(cpp_files_common
     )
 
 # add hpp files to library
-target_include_directories(cpp_files_common PRIVATE ${CMAKE_CURRENT_LIST_DIR}/common/ ${Eigen_INCLUDE_DIRS})
-target_link_libraries(cpp_files_common ${Eigen_LIBRARIES})
+target_include_directories(cpp_files_common PRIVATE 
+                ${CMAKE_CURRENT_LIST_DIR}/common/) 
 
 # link the library 
-target_link_libraries(${LF_MAIN_TARGET} cpp_files_common)
+target_link_libraries(${LF_MAIN_TARGET} cpp_files_common
+                                        ${CMAKE_THREAD_LIBS_INIT} 
+                                        mujoco::mujoco glfw
+                                        "${X_ARM_LIB}")
 
 # make the library visible also for the .lf files
-target_include_directories(${LF_MAIN_TARGET} PRIVATE ${CMAKE_CURRENT_LIST_DIR}/common/)
+target_include_directories(${LF_MAIN_TARGET} PUBLIC 
+                                    "${X_ARM_INCLUDE_PATH}"
+                                    ${CMAKE_CURRENT_LIST_DIR}/common/
+                                    ${CMAKE_CURRENT_LIST_DIR}/common/camera 
+                                    ${CMAKE_CURRENT_LIST_DIR}/common/camera/camera_lib)
 
