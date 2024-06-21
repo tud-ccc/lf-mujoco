1e14101fbc5d915f3f9dd4c8496d7684d08d7af6
PmupHutt <benedict.mehnert@gmail.com>
2024-06-21 10:37:31 +0200

    stay at first position

--- src/xarm.cmake
+++ src/xarm.cmake
@@ -31,6 +31,7 @@ ADD_LIBRARY(cpp_files_common
     ${CMAKE_CURRENT_LIST_DIR}/common/common_vector.cpp
     ${CMAKE_CURRENT_LIST_DIR}/common/common_world_data.cpp
     ${CMAKE_CURRENT_LIST_DIR}/common/common_deacceleration_controller.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/common/common_coordinate_transformation.cpp
     )
 
 # add hpp files to library
